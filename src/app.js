a54495ed56e591899a1167f849e81eca07250afe
J D <me@jdinh.life>
2016-01-05 08:37:13 +1100

    added babel-polyfill

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('babel-polyfill');
+
 var React = require('react'), //eslint-disable-line no-unused-vars
     ReactDOM = require('react-dom'),
     TripBox = require('./app/Trips/tripBox.js'); //eslint-disable-line no-unused-vars
